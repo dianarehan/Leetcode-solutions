1e84756270ddfa2de897340fc06e3cc92473c10f
Diana Rehan <47849049+dianarehan@users.noreply.github.com>
2024-07-17 01:15:32 +0300

    Update Solution.java

--- 11/Solution.java
+++ 11/Solution.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left =0;
+        int right = height.length-1;
+        int maxArea=0;
 
+        for(int i =0; i<height.length;i++){
+            maxArea =Math.max(maxArea,getArea(height,left,right));
+            if(height[left]>height[right]) right--; else left++;
+        }
+        return maxArea;
+    }
+    private int getArea(int [] height, int left, int right){
+        return Math.min(height[left],height[right])*(right-left);
+    }
+        
+    
+}
