8b9f44a2bbe7de79aaad9d59fbe0465f15d13c5f
Diana Rehan <daiana.rehan.dr@gmail.com>
2024-12-16 20:31:13 +0200

    Update Solution.java

--- 70/Solution.java
+++ 70/Solution.java
@@ -7,11 +7,11 @@ class Solution {
     private int dfs(HashMap<Integer, Integer> dp,int n,int step){
         if(step==n) return 1;
         if(step>n) return 0;
-        if(dp.containsKey(step))
-            return dp.get(step);
-        int twoStep= dfs(dp,n,step+2);
-        int oneStep=dfs(dp,n,step+1);
-        dp.put(step,twoStep+oneStep);
+        if(!dp.containsKey(step)){
+            int twoStep= dfs(dp,n,step+2);
+            int oneStep=dfs(dp,n,step+1);
+            dp.put(step,twoStep+oneStep);
+            }
         return dp.get(step);
     }
 }
